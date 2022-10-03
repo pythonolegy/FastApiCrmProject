bf2cbf3c2dab9c9704ab248d23b8faf18ee519e9
okachalin@forsalescrm.xyz <okachalin@forsalescrm.xyz>
2022-10-03 04:09:41 +0200

    imports and server init

---
+++ app/server.py
@@ -0,0 +1,29 @@
+import schedule
+from main import *
+
+import uvicorn
+from fastapi import FastAPI, status
+from fastapi.responses import JSONResponse
+import logging
+
+from fastapi.middleware.cors import CORSMiddleware
+
+from server_alert import send_alert_message
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(filename)s %(levelname)s: %(message)s')
+
+
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
