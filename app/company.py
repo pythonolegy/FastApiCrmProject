3dcf3fdc6f2b7c796837deff359bfb36ca979c37
pythonolegy <olegkachalin20@gmail.com>
2022-10-03 04:33:57 +0200

    added get_company_details

--- app/company.py
+++ app/company.py
@@ -17,3 +17,24 @@ class CompanyService:
         self.company_action = None
         self.company_pk = None
         self.action = 'update'
+
+    async def init(self):
+        self.company_details = await self.get_company_details()
+        self.company_action = self.get_company_action()
+        self.company_pk = await self.company_action()
+
+    async def get_company_details(self):
+        requisite = f'crm.requisite.list?FILTER[ENTITY_TYPE_ID]=4&FILTER[RQ_INN]={self.data.ContractorData.INN}'
+        company = 'crm.company.list?FILTER[ID]=$result[requisite][0][ENTITY_ID]&SELECT[]=*&SELECT[]=UF_*'
+        bank_details = 'crm.requisite.bankdetail.list?FILTER[ENTITY_ID]=$result[requisite][0][ID]'
+        addresses = 'crm.address.list?FILTER[ENTITY_ID]=$result[company][0][ID]&FILTER[ENTITY_TYPE_ID]=4'
+        return await b.call_batch({
+            'halt': 0,
+            'cmd': {
+                'requisite': requisite,
+                'company': company,
+                'bank_details': bank_details,
+                'addresses': addresses
+            }
+        })
+
