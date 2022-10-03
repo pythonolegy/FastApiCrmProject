e5f1596964b5434263f32fb99a59cdd06d92c883
pythonolegy <olegkachalin20@gmail.com>
2022-10-03 04:43:59 +0200

    add get_deal_action

--- app/deal.py
+++ app/deal.py
@@ -23,3 +23,35 @@ class DealService:
         self.deal_list = None
         self.action = 'update'
         self.company_pk = company.CompanyService(self.data).company_pk
+
+    async def init(self):
+        self.deal_action = await self.get_deal_action()
+        self.deal_pk = await self.deal_action()
+        await self.set_products_to_deal()
+        return {'result': 200}
+
+    async def get_deal_action(self) -> Callable:
+        # The first case --- find a deal by the INN of the company and the account number of the transaction
+        self.deal = await self.get_deal()
+        if self.exist_deal_details():
+            self.deal_pk = self.deal[0]['ID']
+            self.action = 'update'
+            return self.update_deal
+
+        # The second case --- the transaction is based on the Account Number from ...
+        self.deal = await self.get_deal_by_field()
+        if self.exist_deal_details():
+            self.deal_pk = self.deal[0]['ID']
+            self.action = 'update'
+            return self.update_deal
+
+        # The third case --- the old company already had transactions with some information (without the Account Number)
+        self.deal = await self.get_deal_id_without_field()
+        if self.exist_deal_details():
+            self.deal_pk = self.deal[0]['ID']
+            self.action = 'update'
+            return self.update_deal
+
+        self.deal_pk = ''
+        self.action = 'add'
+        return self.add_deal
