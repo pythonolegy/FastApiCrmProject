bd945db07f609f890cbf1fe7830f6a21248afc23
pythonolegy <olegkachalin20@gmail.com>
2022-10-03 04:58:54 +0200

    add schedule jobs logic(tested)

--- app/schedule.py
+++ app/schedule.py
@@ -9,3 +9,8 @@ import requests_saver
 tz = ''
 scheduler = BackgroundScheduler()
 scheduler.configure(timezone=tz)
+
+scheduler.add_job(func=email_sender.main, trigger='cron', year='*', month='*', hour='23', minute=0)
+scheduler.add_job(func=requests_saver.clear_file_requests, trigger='cron', year='*', month='*', hour='23', minute=30)
+logging.getLogger('apscheduler').setLevel(logging.DEBUG)
+scheduler.start()
