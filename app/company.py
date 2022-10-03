0c580e790968c143af831c743227dbef9a1fb553
pythonolegy <olegkachalin20@gmail.com>
2022-10-03 04:38:16 +0200

    added put and update functions

--- app/company.py
+++ app/company.py
@@ -86,6 +86,13 @@ class CompanyService:
             return [{"VALUE": emails[0]}, {"VALUE": emails[1]}]
         return [{"VALUE": data}]
 
+    async def add_company(self) -> str:
+        company_id = await self.put_company()
+        requisite_id = await self.put_company_requisite(company_id)
+        await self.put_company_banking_details(requisite_id)
+        await self.put_company_address(company_id)
+        return company_id
+
     async def get_user(self):
         return await b.call(
             'user.get',
@@ -102,4 +109,79 @@ class CompanyService:
             return False
         return True
 
+    async def put_company(self, company_pk: str = '') -> str:
+        user = await self.get_user()
+        user_id = ''
+        if self.action == 'add':
+            user_id = '93'
+            if self.exist_user(user):
+                user_id = user[0]['ID']
+        logging.info(f"COMPANY USER - {user}")
+        logging.info(f"COMPANY USER - {user_id}")
+        return await b.call(
+            f'crm.company.{self.action}',
+            {
+                'ID': company_pk,
+                'fields': {
+                    'TITLE': self.data.ContractorData.Title,
+                    settings.COMPANY_FIELDS['CONTRACT_NUMBER']: self.data.Contract,
+                    # settings.COMPANY_FIELDS["SPECIFICATION_NUMBER"]: self.data.AdditionalData.SpecNum,
+                    "PHONE": self.get_contact_data_bx('PHONE'),
+                    "EMAIL": self.get_contact_data_bx('EMAIL'),
+                    settings.COMPANY_FIELDS["SIGNATORY_POSITION"]: self.data.ContractorData.CardPosition,
+                    'ASSIGNED_BY_ID': user_id
+                }
+            }
+        )
 
+    async def put_company_requisite(self, company_pk, requisite_id: str = '') -> str:
+        return await b.call(
+            f'crm.requisite.{self.action}',
+            {
+                "ID": requisite_id,
+                'fields': {
+                    "ENTITY_ID": company_pk,
+                    "ENTITY_TYPE_ID": 4,
+                    "PRESET_ID": 1,
+                    "NAME": self.data.ContractorData.Title,
+                    "RQ_INN": self.data.ContractorData.INN,
+                    "RQ_KPP": self.data.ContractorData.KPP,
+                    "RQ_OKPO": self.data.ContractorData.OKPO,
+                    "RQ_DIRECTOR": self.data.ContractorData.ManegerFullName,
+                }
+            }
+        )
+
+    async def put_company_banking_details(self, requisite_id, banking_id: str = ''):
+        name = self.data.ContractorData.Bank
+        if not self.data.ContractorData.Bank:
+            name = 'Не заполнено'
+        await b.call(
+            f'crm.requisite.bankdetail.{self.action}',
+            {
+                'ID': banking_id,
+                'fields': {
+                    "ENTITY_ID": requisite_id,
+                    "NAME": name,
+                    "RQ_BIK": self.data.ContractorData.BIK,
+                    "RQ_ACC_NUM": self.data.ContractorData.SettlementAccount,
+                    "RQ_COR_ACC_NUM": self.data.ContractorData.CorrespondentAccount
+                }
+            }
+        )
+
+    async def put_company_address(self, company_id: str):
+        await b.call(
+            f'crm.address.add',
+            self.get_company_addresses(company_id)
+        )
+
+    async def update_company(self) -> str:
+        company_id = self.company_details['company'][0]['ID']
+        requisite_id = self.company_details['requisite'][0]['ID']
+        banking_id = self.company_details['bank_details'][0]['ID']
+        await self.put_company(company_id)
+        await self.put_company_requisite(company_id, requisite_id)
+        await self.put_company_address(company_id)
+        await self.put_company_banking_details(requisite_id, banking_id)
+        return company_id
